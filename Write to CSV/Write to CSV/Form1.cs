264e055f2f8efbcf9fc84d2aba4699b1b30f76eb
Kristoffer <kriss0706@hotmail.com>
2020-08-14 13:51:58 +0200

    afsasf

--- Write to CSV/Write to CSV/Form1.cs
+++ Write to CSV/Write to CSV/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CsvHelper;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,42 @@ namespace Write_to_CSV
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public class Foo
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var records = new List<Foo>
+            {
+                new Foo { Id = 1, Name = "John", Age = 21 },
+                new Foo { Id = 2, Name = "Matthew", Age = 18 }
+            };
+
+            using (var writer = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Eksport fra Flexi.csv"))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(records);
+
+
+                writer.Flush();
+            }
+        }
+
+        public class Test
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
         }
     }
 }
